6c614f7bb6ddef23360663dab3b59c9dc0bd523d
Shema Alphonse <ashema330@gmail.com>
2026-01-20 18:21:26 +0200

    feat(model): adding entities and constructor for department

---
+++ src/main/java/hospital/hospital_management_system2/model/Department.java
@@ -0,0 +1,38 @@
+package hospital.hospital_management_system2.model;
+
+public class Department {
+    private int deptId;
+    private String deptName;
+    private int locationFloor;
+
+    public Department(int locationFloor, String deptName) {
+        this.locationFloor = locationFloor;
+        this.deptName = deptName;
+    }
+
+    public Department(int deptId, int locationFloor, String deptName) {
+        this.deptId = deptId;
+        this.locationFloor = locationFloor;
+        this.deptName = deptName;
+    }
+
+    public Department(int deptId) {
+        this.deptId = deptId;
+    }
+
+    public int getDeptId() {return deptId;}
+    public void setDeptId(int deptId) {this.deptId = deptId;}
+    public int getLocationFloor() {return locationFloor;}
+    public void setLocationFloor(int locationFloor) {this.locationFloor = locationFloor;}
+    public String getDeptName() {return deptName;}
+    public void setDeptName(String deptName) {this.deptName = deptName;}
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "deptId=" + deptId +
+                ", deptName='" + deptName + '\'' +
+                ", locationFloor=" + locationFloor +
+                '}';
+    }
+}
