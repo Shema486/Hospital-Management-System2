5b41226db27d796f29c67131f0a88ea7cc6ae60a
Shema Alphonse <ashema330@gmail.com>
2026-01-21 08:22:26 +0200

    feat: updating mainApp view and controller

--- src/main/java/hospital/hospital_management_system2/MainAppController.java
+++ src/main/java/hospital/hospital_management_system2/MainAppController.java
@@ -1,14 +1,41 @@
 package hospital.hospital_management_system2;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class MainAppController {
     @FXML
     private Label welcomeText;
+    @FXML private BorderPane mainPane;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML public void showDepartment(){
+        loadView("DepartmentView.fxml");
+    }
+    private void loadView(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/hospital/hospital_management_system2/" + fxmlFile));
+            Parent view = loader.load();
+            if (mainPane != null) {
+                mainPane.setCenter(view);
+            } else {
+                System.err.println("mainPane is null!");
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading view: " + fxmlFile);
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module hospital.hospital_management_system2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
     requires javafx.web;
+
+    requires java.sql;
+
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -10,8 +15,9 @@ module hospital.hospital_management_system2 {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
-    requires java.sql;
 
     opens hospital.hospital_management_system2 to javafx.fxml;
+    opens hospital.hospital_management_system2.controller to javafx.fxml;
+
     exports hospital.hospital_management_system2;
 }
